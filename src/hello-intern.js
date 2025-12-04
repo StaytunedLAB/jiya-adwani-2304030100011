8ee07742e9a8309236321c53b7b4bccab3c605d1
Jiya <adwanijiya32@gmail.com>
2025-12-04 15:44:47 +0530

    add technolojiya

--- src/hello-intern.js
+++ src/hello-intern.js
@@ -7,3 +7,4 @@ console.log("-----------------------------------------");
 console.log("Your environment is ready. You can now start coding!");
 console.log("Happy Coding! 🎉");
 console.log("Hello World! 🌍");
+console.log("Technolojiya🤝");
